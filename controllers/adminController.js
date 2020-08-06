b84b9212cee41641a49a95e4757344afc9da4632
Denny <denny1011133@gmail.com>
2020-08-06 14:05:37 +0800

    Add admin all_music index page

---
+++ controllers/adminController.js
@@ -0,0 +1,7 @@
+const adminController = {
+  getAllMusic: (req, res) => {
+    return res.render('admin/allMusic')
+  }
+}
+
+module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const musicController = require('../controllers/musicController.js')
+const adminController = require('../controllers/adminController.js')
+
+
+
 module.exports = app => {
 
-  app.get('/', (req, res) => res.redirect('/all-music'))
-  app.get('/all-music', musicController.getAllMusic)
+  app.get('/', (req, res) => res.redirect('/all_music'))
+  app.get('/all_music', musicController.getAllMusic)
+
+  app.get('/admin', (req, res) => res.redirect('/admin/all_music'))
+  app.get('/admin/all_music', adminController.getAllMusic)
+
+
 }
