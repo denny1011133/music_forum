edbeb3327badda3c232d4a535f98d16a42e4e3ec
Denny <denny1011133@gmail.com>
2020-08-06 19:30:52 +0800

    Add admin albums index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,15 @@
+const db = require('../models')
+const Album = db.Album
+
+
+
+
 const adminController = {
   getAllMusic: (req, res) => {
-    return res.render('admin/allMusic')
+    return Album.findAll({ raw: true })
+      .then(albums => {
+        return res.render('admin/allMusic', { albums })
+      })
   }
 }
 
