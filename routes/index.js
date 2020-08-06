35a1b2f3754468a9020378b3c73cb1088637dcf4
Denny <denny1011133@gmail.com>
2020-08-06 15:48:04 +0800

    Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,28 @@ const musicController = require('../controllers/musicController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 
-
-
 module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next()
+      }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/', (req, res) => res.redirect('/all_music'))
-  app.get('/all_music', musicController.getAllMusic)
+  app.get('/', authenticated, (req, res) => res.redirect('/all_music'))
+  app.get('/all_music', authenticated, musicController.getAllMusic)
 
-  app.get('/admin', (req, res) => res.redirect('/admin/all_music'))
-  app.get('/admin/all_music', adminController.getAllMusic)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/all_music'))
+  app.get('/admin/all_music', authenticatedAdmin, adminController.getAllMusic)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
