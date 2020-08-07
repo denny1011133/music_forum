6cadc1958bc7b1b6751160d0105ffd7b2d9648ab
Denny <denny1011133@gmail.com>
2020-08-07 19:12:02 +0800

    Add: Pagination

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,17 +1,26 @@
 const db = require('../models')
 const Album = db.Album
 const Category = db.Category
-
+const pageLimit = 10
 const albumController = {
   getAllAlbums: (req, res) => {
+    let offset = 0
     let whereQuery = {}
     let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery['CategoryId'] = categoryId
     }
-    Album.findAll({ include: Category, where: whereQuery }).then(albums => {
-      const data = albums.map(a => ({
+    Album.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit }).then(result => {
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
+      const data = result.rows.map(a => ({
         ...a.dataValues,
         description: a.dataValues.description.substring(0, 40),
         categoryName: a.Category.name
@@ -23,7 +32,11 @@ const albumController = {
         return res.render('allAlbums', {
           albums: data,
           categories,
-          categoryId
+          categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
         })
       })
     })
