2cb5589a8d017cdbab57bd7bf7b8aafbf588f962
Denny <denny1011133@gmail.com>
2020-08-07 15:45:19 +0800

    Add albums show page

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,6 +1,24 @@
+const db = require('../models')
+const Album = db.Album
+const Category = db.Category
+
 const albumController = {
   getAllAlbums: (req, res) => {
-    return res.render('allAlbums')
+    Album.findAll({ include: Category }).then(albums => {
+      const data = albums.map(a => ({
+        ...a.dataValues,
+        description: a.dataValues.description.substring(0, 40),
+        categoryName: a.Category.name
+      }))
+      return res.render('allAlbums', {
+        albums: data
+      })
+    })
+  },
+  getAlbum: (req, res) => {
+    return Album.findByPk(req.params.id, { include: Category }).then(album => {
+      return res.render('album', { album: album.toJSON() })
+    })
   }
 }
 module.exports = albumController
--- routes/index.js
+++ routes/index.js
@@ -48,4 +48,5 @@ module.exports = (app, passport) => {
   app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
   app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
+  app.get('/albums/:id', authenticated, albumController.getAlbum)
 }
